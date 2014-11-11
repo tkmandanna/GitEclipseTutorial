876b84af8885be680b18e044a7828c18bb0d8e7f
Mandanna Thekkada <mandanna.thekkada@gmail.com>
2014-11-11 15:33:16 -0500

    BarbillCalculator

---
+++ CircDebt/src/com/mcswiggins/onerepublic/BarBill.java
@@ -0,0 +1,14 @@
+package com.mcswiggins.onerepublic;
+
+	
+public class BarBill
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		CalcBarBill cbb=new CalcBarBill();
+		cbb.calcBill();
+		//System.out.println("hello world\n\n");
+		
+	}
+}
---
+++ CircDebt/src/com/mcswiggins/onerepublic/Barhopper.java
@@ -0,0 +1,16 @@
+package com.mcswiggins.onerepublic;
+
+class Barhopper
+{
+	String name;
+	double paid;
+	double net_amt;
+	
+	Barhopper(String name)
+	{
+		this.name=name;
+		this.net_amt=0;
+		this.net_amt=0;
+	}
+}
+
